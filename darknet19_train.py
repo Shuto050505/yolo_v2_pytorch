7d573b9fc539123b21c819576092d391be78b6c7
Shuto.Tsuchiya <tsuchiya0505005@gmail.com>
2018-03-22 23:28:02 +0900

    d19_1

--- darknet19_train.py
+++ darknet19_train.py
@@ -1,90 +1,110 @@
 import time
 import cv2
 import numpy as np
-# import chainer
 import glob
 import os
-# from chainer import serializers, optimizers, Variable, cuda
-# import chainer.functions as F
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+
 from darknet19 import *
 from lib.image_generator import *
 
-# hyper parameters
-input_height, input_width = (224, 224)
-item_path = "./items"
-background_path = "./backgrounds"
-label_file = "./data/label.txt"
-backup_path = "backup"
-batch_size = 32
-max_batches = 3000
-learning_rate = 0.001
-lr_decay_power = 4
-momentum = 0.9
-weight_decay = 0.0005
 
-# load image generator
-print("loading image generator...")
-generator = ImageGenerator(item_path, background_path)
+def main():
+    # hyper parameters
+    input_height, input_width = (224, 224)
+    item_path = "./items"
+    background_path = "./backgrounds"
+    label_file = "./data/label.txt"
+    backup_path = "backup"
+    batch_size = 32
+    max_batches = 3000
+    learning_rate = 0.001
+    lr_decay_power = 4
+    momentum = 0.9
+    weight_decay = 0.0005
 
-with open(label_file, "r") as f:
-    labels = f.read().strip().split("\n")
+    # load image generator
+    print("loading image generator...")
+    generator = ImageGenerator(item_path, background_path)
 
-# load model
-print("loading model...")
-model = Darknet19Predictor(Darknet19())
-backup_file = "%s/backup.model" % (backup_path)
-if os.path.isfile(backup_file):
-    serializers.load_hdf5(backup_file, model) # load saved model
-model.predictor.train = True
-cuda.get_device(0).use()
-model.to_gpu() # for gpu
+    with open(label_file, "r") as f:
+        labels = f.read().strip().split("\n")
 
-optimizer = optimizers.MomentumSGD(lr=learning_rate, momentum=momentum)
-optimizer.use_cleargrads()
-optimizer.setup(model)
-optimizer.add_hook(chainer.optimizer.WeightDecay(weight_decay))
+    # load model
+    print("loading model...")
+    model = Darknet19(num_classes=10, phase='train')
 
-# start to train
-print("start training")
-for batch in range(max_batches):
-    # generate sample
-    x, t = generator.generate_samples(
-        n_samples=batch_size,
-        n_items=1,
-        crop_width=input_width,
-        crop_height=input_height,
-        min_item_scale=0.3,
-        max_item_scale=1.3,
-        rand_angle=25,
-        minimum_crop=0.8,
-        delta_hue=0.01,
-        delta_sat_scale=0.5,
-        delta_val_scale=0.5
-    )
-    x = Variable(x)
-    one_hot_t = []
-    for i in range(len(t)):
-        one_hot_t.append(t[i][0]["one_hot_label"])
-    x.to_gpu()
-    one_hot_t = np.array(one_hot_t, dtype=np.float32)
-    one_hot_t = Variable(one_hot_t)
-    one_hot_t.to_gpu()
+    # Load Weight
+    # weight_path = ''
+    # model.load_state_dict(weight_path)
 
-    y, loss, accuracy = model(x, one_hot_t)
-    print("[batch %d (%d images)] learning rate: %f, loss: %f, accuracy: %f" % (batch+1, (batch+1) * batch_size, optimizer.lr, loss.data, accuracy.data))
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+    criterion = nn.MSELoss()
 
-    optimizer.zero_grads()
-    loss.backward()
+    def lr_scheduler(optimizer, batch, lr_decay_power):
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = learning_rate * (1 - batch / max_batches) ** lr_decay_power
+        return optimizer
 
-    optimizer.lr = learning_rate * (1 - batch / max_batches) ** lr_decay_power # Polynomial decay learning rate
-    optimizer.update()
+    # start to train
+    print("start training")
+    for batch in range(max_batches):
+        # generate sample
+        x, t = generator.generate_samples(
+            n_samples=batch_size,
+            n_items=1,
+            crop_width=input_width,
+            crop_height=input_height,
+            min_item_scale=0.3,
+            max_item_scale=1.05,
+            rand_angle=25,
+            minimum_crop=0.8,
+            delta_hue=0.01,
+            delta_sat_scale=0.5,
+            delta_val_scale=0.5
+        )
+        x = torch.from_numpy(x)
+        #     x.cuda()
+        x = Variable(x)
 
-    # save model
-    if (batch+1) % 1000 == 0:
-        model_file = "%s/%s.model" % (backup_path, batch+1)
-        print("saving model to %s" % (model_file))
-        serializers.save_hdf5(model_file, model)
-        serializers.save_hdf5(backup_file, model)
+        one_hot_t = []
+        for i in range(len(t)):
+            one_hot_t.append(t[i][0]["one_hot_label"])
+        one_hot_t = np.array(one_hot_t, dtype=np.float32)
+        one_hot_t = torch.from_numpy(one_hot_t)
+        #     one_hot_t.cuda()
+        one_hot_t = Variable(one_hot_t)
 
-print("saving model to %s/darknet19_final.model" % (backup_path))
-serializers.save_hdf5("%s/darknet19_final.model" % (backup_path), model)
+        optimizer.zero_grad()
+        y = model(x)
+        loss = criterion(y, one_hot_t)
+        loss.backward()
+        optimizer.step()
+
+        _, pred = torch.max(y.data, 1)
+        _, answ = torch.max(one_hot_t.data, 1)
+        accuracy = (pred == answ).sum() / batch_size
+
+        print("[batch %d (%d images)] learning rate: %f, loss: %f, accuracy: %f" % (
+            batch + 1, (batch + 1) * batch_size, optimizer.param_groups[0]['lr'], loss, accuracy))
+
+        # update lr
+        optimizer = lr_scheduler(optimizer, batch, lr_decay_power)  # Polynomial decay learning rate
+
+        # save model
+        if (batch + 1) % 1000 == 0:
+            model_file = "%s/%s.pth" % (backup_path, batch + 1)
+            print("saving model to %s" % (model_file))
+            torch.save(model.state_dict(), model_file)
+
+    print("saving model to %s/darknet19_final.model" % (backup_path))
+    torch.save(model.state_dict(), "%s/darknet19_final.model" % (backup_path))
+
+
+if __name__ == '__main__':
+    main()
